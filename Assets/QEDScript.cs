219fcec06d9266ce5e46c0ca4a8b6afc8812fd76
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2022-12-03 20:30:10 -0500

    [V1.0] Initial commit

---
+++ Assets/QEDScript.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class QEDScript : MonoBehaviour
+{
+    [SerializeField]
+    private QEDCard[] _buttons;
+    [SerializeField]
+    private Renderer _backing;
+
+    private int _id = ++_idc;
+    private static int _idc;
+    private bool _isSolved;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    private QEDCard.Shape[] _shapes;
+    private int[] _hls;
+    private QEDCard.Symbol[] _symbols;
+
+    private static readonly string[] _hlNames = new string[] { "White on White", "White on Black", "Black on White", "Black on Black" };
+    private static readonly Color _buttonColor = new Color32(97, 65, 180, 255);
+    private struct CardData
+    {
+        public QEDCard.Shape Shape;
+        public int Hl;
+        public QEDCard.Symbol Symbol;
+
+        public static CardData Rng()
+        {
+            return new CardData()
+            {
+                Shape = (QEDCard.Shape)Random.Range(0, 4),
+                Hl = Random.Range(0, 4),
+                Symbol = new QEDCard.Symbol()
+                {
+                    Shape = (QEDCard.Shape)Random.Range(0, 4),
+                    Fill = (QEDCard.Fill)Random.Range(0, 4)
+                }
+            };
+        }
+
+        public int Part;
+    }
+    private void Generate()
+    {
+        //_shapes = Enumerable.Repeat(1, 16).Select(_ => (QEDCard.Shape)Random.Range(0, 4)).ToArray();
+        //_hls = Enumerable.Repeat(2, 16).Select(_ => Random.Range(0, 4)).ToArray();
+        //_symbols = Enumerable.Repeat(3, 16).Select(_ => new QEDCard.Symbol() { Shape = (QEDCard.Shape)Random.Range(0, 4), Fill = (QEDCard.Fill)Random.Range(0, 4) }).ToArray();
+        _shapes = new QEDCard.Shape[16];
+        _hls = new int[16];
+        _symbols = new QEDCard.Symbol[16];
+
+        bool[] set = new bool[4].Select(_ => Random.Range(0, 1) == 1).ToArray();
+
+        // shape, hl, shape, fill
+        // same, 2pair, different
+        int[][] qedmodes = new int[4][];
+        do
+        {
+            for(int i = 0; i < 4; ++i)
+            {
+                if(set[i])
+                    qedmodes[i] = Enumerable.Repeat(Random.Range(1, 4), 3).ToArray();
+                else
+                    qedmodes[i] = Enumerable.Range(1, 3).OrderBy(_ => Random.value).ToArray();
+            }
+        }
+        while(new int[] { 0, 1, 2 }.Any(i => new int[] { 0, 1, 2, 3 }.All(j => qedmodes[j][i] == 1)));
+
+        CardData[][] qeds = new CardData[3][];
+        for(int i = 0; i < 3; ++i)
+            qeds[i] = new CardData[4];
+
+        for(int q = 0; q < 3; ++q)
+        {
+            QEDCard.Shape[] tshapes;
+            if(qedmodes[0][q] == 1)
+                tshapes = Enumerable.Repeat((QEDCard.Shape)Random.Range(0, 4), 4).ToArray();
+            else if(qedmodes[0][q] == 2)
+                tshapes = Enumerable.Range(0, 4).Select(i => (QEDCard.Shape)i).OrderBy(_ => Random.value).Take(2).SelectMany(i => new QEDCard.Shape[] { i, i }).ToArray();
+            else if(qedmodes[0][q] == 3)
+                tshapes = Enumerable.Range(0, 4).Select(i => (QEDCard.Shape)i).OrderBy(_ => Random.value).ToArray();
+            else
+                throw new Exception("shape mode");
+
+            int[] thls;
+            if(qedmodes[1][q] == 1)
+                thls = Enumerable.Repeat(Random.Range(0, 4), 4).ToArray();
+            else if(qedmodes[1][q] == 2)
+                thls = Enumerable.Range(0, 4).OrderBy(_ => Random.value).Take(2).SelectMany(i => new int[] { i, i }).ToArray();
+            else if(qedmodes[1][q] == 3)
+                thls = Enumerable.Range(0, 4).OrderBy(_ => Random.value).ToArray();
+            else
+                throw new Exception("hl mode");
+
+            QEDCard.Shape[] tshapes2;
+            if(qedmodes[2][q] == 1)
+                tshapes2 = Enumerable.Repeat((QEDCard.Shape)Random.Range(0, 4), 4).ToArray();
+            else if(qedmodes[2][q] == 2)
+                tshapes2 = Enumerable.Range(0, 4).Select(i => (QEDCard.Shape)i).OrderBy(_ => Random.value).Take(2).SelectMany(i => new QEDCard.Shape[] { i, i }).ToArray();
+            else if(qedmodes[2][q] == 3)
+                tshapes2 = Enumerable.Range(0, 4).Select(i => (QEDCard.Shape)i).OrderBy(_ => Random.value).ToArray();
+            else
+                throw new Exception("shape mode 2");
+
+            QEDCard.Fill[] tfills;
+            if(qedmodes[3][q] == 1)
+                tfills = Enumerable.Repeat((QEDCard.Fill)Random.Range(0, 4), 4).ToArray();
+            else if(qedmodes[3][q] == 2)
+                tfills = Enumerable.Range(0, 4).Select(i => (QEDCard.Fill)i).OrderBy(_ => Random.value).Take(2).SelectMany(i => new QEDCard.Fill[] { i, i }).ToArray();
+            else if(qedmodes[3][q] == 3)
+                tfills = Enumerable.Range(0, 4).Select(i => (QEDCard.Fill)i).OrderBy(_ => Random.value).ToArray();
+            else
+                throw new Exception("fill mode");
+
+            for(int i = 0; i < 4; ++i)
+            {
+                qeds[q][i] = new CardData()
+                {
+                    Shape = tshapes[i],
+                    Hl = thls[i],
+                    Symbol = new QEDCard.Symbol()
+                    {
+                        Shape = tshapes2[i],
+                        Fill = tfills[i]
+                    },
+                    Part = q + 1
+                };
+            }
+        }
+
+        CardData[] r = Enumerable.Repeat(0, 4).Select(_ => CardData.Rng()).ToArray();
+        CardData[] cards = qeds.SelectMany(q => q).Concat(r).ToArray();
+        cards = cards.OrderBy(_ => Random.value).ToArray();
+        _shapes = cards.Select(d => d.Shape).ToArray();
+        _hls = cards.Select(d => d.Hl).ToArray();
+        _symbols = cards.Select(d => d.Symbol).ToArray();
+
+        for(int i = 0; i < 16; ++i)
+        {
+            Log(string.Format("Button {0}: {1}, {2}, {3}", i, _shapes[i], _hlNames[_hls[i]], _symbols[i]));
+
+            _buttons[i].ButtonShape = _shapes[i];
+            _buttons[i].BaseColor = _buttonColor;
+            _buttons[i].HighlightColor = _hls[i] % 2 == 1 ? Color.white : Color.black;
+            _buttons[i].SymbolBaseColor = Color.black;
+            _buttons[i].SymbolHighlightColor = _hls[i] / 2 == 1 ? Color.white : Color.black;
+            _buttons[i].ShownSymbol = _symbols[i];
+
+            int j = i;
+            _buttons[i].Press += () => Press(j);
+        }
+
+        Log(string.Format("A possible solution would be: ({0}), ({1}), ({2})",
+            Enumerable.Range(0,16).Where(i => cards[i].Part == 1).Join(", "),
+            Enumerable.Range(0,16).Where(i => cards[i].Part == 2).Join(", "),
+            Enumerable.Range(0,16).Where(i => cards[i].Part == 3).Join(", ")
+            ));
+
+        //TODO: Generate uniquely?
+    }
+
+    private readonly List<int> _selections = new List<int>(12);
+    private readonly List<Color> _usedColors = new List<Color>(3);
+    private readonly List<Color> _unusedColors = new List<Color>(3) { Color.red, Color.green, Color.blue };
+    private readonly List<int[]> _qedRules = new List<int[]>(3);
+    private void Press(int ix)
+    {
+        Log("Pressed button " + ix);
+        if(_isSolved)
+            return;
+
+        if(_usedColors.Count == 0)
+        {
+            int c = Random.Range(0, 3);
+            _usedColors.Add(_unusedColors[c]);
+            _unusedColors.RemoveAt(c);
+        }
+        if(_selections.Contains(ix))
+        {
+            int pos = _selections.IndexOf(ix);
+            if(_selections.Count >= 8 && pos >= 4 && pos < 8)
+            {
+                foreach(int btn in _selections.Skip(4).Take(4))
+                    _buttons[btn].BaseColor = _buttonColor;
+                _selections.RemoveRange(4, 4);
+                _unusedColors.Add(_usedColors[1]);
+                _usedColors.RemoveAt(1);
+                _qedRules.RemoveAt(1);
+            }
+            else if(_selections.Count >= 4 && pos < 4)
+            {
+                foreach(int btn in _selections.Take(4))
+                    _buttons[btn].BaseColor = _buttonColor;
+                _selections.RemoveRange(0, 4);
+                _unusedColors.Add(_usedColors[0]);
+                _usedColors.RemoveAt(0);
+                _qedRules.RemoveAt(0);
+            }
+            else
+            {
+                _buttons[ix].BaseColor = _buttonColor;
+                _selections.RemoveAt(pos);
+            }
+        }
+        else
+        {
+            if(_selections.Count % 4 == 3)
+            {
+                bool valid = IsQED(_selections.TakeLast(3).Concat(new int[] { ix }).ToArray());
+
+                if(!valid)
+                {
+                    Log("That does not form a Q.E.D. Strike!");
+                    GetComponent<KMBombModule>().HandleStrike();
+                }
+                else
+                {
+                    Log("That is a Q.E.D.");
+                    if(_selections.Count == 11)
+                    {
+                        bool setValid = IsSET();
+
+                        if(!setValid)
+                        {
+                            Log("Those Q.E.D.s do not form a S.E.T. Strike!");
+                            GetComponent<KMBombModule>().HandleStrike();
+                        }
+                        else
+                        {
+                            Log("That is a S.E.T. Module solved.");
+                            _isSolved = true;
+                            _buttons[ix].BaseColor = _usedColors.Last();
+                            _backing.material.color = new Color32(14, 118, 35, 255);
+                            GetComponent<KMAudio>().PlaySoundAtTransform("solve", transform);
+                            GetComponent<KMBombModule>().HandlePass();
+                        }
+                    }
+                    else
+                    {
+                        _selections.Add(ix);
+                        _buttons[ix].BaseColor = _usedColors.Last();
+
+                        int c = Random.Range(0, _unusedColors.Count);
+                        _usedColors.Add(_unusedColors[c]);
+                        _unusedColors.RemoveAt(c);
+                    }
+                }
+            }
+            else
+            {
+                _selections.Add(ix);
+                _buttons[ix].BaseColor = _usedColors.Last();
+            }
+        }
+    }
+
+    private bool IsSET()
+    {
+        bool[] good = new bool[4];
+
+        for(int i = 0; i < 4; ++i)
+        {
+            int[] s = new int[] { 0, 1, 2 }.Select(q => _qedRules[q][i]).ToArray();
+            if(s.Distinct().Count() == 3 || s.Distinct().Count() == 1)
+                good[i] = true;
+        }
+
+        if(good.All(i => i))
+            return true;
+        else
+        {
+            if(!good[0])
+                Log("Bad S.E.T. of button shapes.");
+            if(!good[0])
+                Log("Bad S.E.T. of highlight rules.");
+            if(!good[0])
+                Log("Bad S.E.T. of shape fills.");
+            if(!good[0])
+                Log("Bad S.E.T. of shapes.");
+            return false;
+        }
+    }
+
+    private bool IsQED(int[] cards)
+    {
+        int[] rules = new int[4];
+
+        int[][] s = new int[][]
+        {
+            cards.Select(i => (int)_shapes[i]).ToArray(),
+            cards.Select(i => _hls[i]).ToArray(),
+            cards.Select(i => (int)_symbols[i].Fill).ToArray(),
+            cards.Select(i => (int)_symbols[i].Shape).ToArray()
+        };
+        for(int i = 0; i < s.Length; ++i)
+        {
+            if(s[i].Distinct().Count() == 4)
+                rules[i] = 1;
+            else if(s[i].Distinct().Count() == 2 && s[i].Count(sp => sp == s[i][0]) == 2)
+                rules[i] = 2;
+            else if(s[i].Distinct().Count() == 1)
+                rules[i] = 3;
+        }
+
+        if(rules.All(i => i != 0))
+        {
+            _qedRules.Add(rules);
+            return true;
+        }
+        else
+        {
+            if(rules[0] == 0)
+                Log("Bad button shapes.");
+            if(rules[1] == 0)
+                Log("Bad highlight rules.");
+            if(rules[2] == 0)
+                Log("Bad shape fills.");
+            if(rules[3] == 0)
+                Log("Bad shapes.");
+            return false;
+        }
+    }
+
+    private void Log(string v)
+    {
+#if UNITY_EDITOR
+        Debug.Log("[Q.E.D. #" + _id + "] " + v);
+#endif
+    }
+}
